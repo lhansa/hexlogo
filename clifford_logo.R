d56ea30bc9c60d74f146bed12100509fc2044cd3
Leonardo Hansa <hansa.leonardo@gmail.com>
2020-06-23 22:08:45 +0200

    en negro

--- clifford_logo.R
+++ clifford_logo.R
@@ -6,6 +6,8 @@ library(hexSticker)
 library(cowplot)
 library(hexSticker)
 
+## Themes ------------------------------------------------------
+
 opt = theme(legend.position  = "none",
             panel.background = element_rect(fill="white"),
             axis.ticks       = element_blank(),
@@ -23,6 +25,18 @@ theme_icon <- function () {
     )
 }
 
+theme_blicon <- function () {
+  theme_void() + 
+    theme(
+      panel.background = element_rect(fill = "transparent", colour = NA), 
+      plot.background = element_rect(fill = "black", colour = NA), 
+      legend.background = element_rect(fill = "transparent", colour = NA), 
+      legend.box.background = element_rect(fill = "transparent", colour = NA)
+    )
+}
+
+# Rcpp --------------------------------------------------------------------
+
 cppFunction('DataFrame createTrajectory(int n, double x0, double y0, 
             double a, double b, double c, double d) {
             // create the columns
@@ -39,6 +53,8 @@ cppFunction('DataFrame createTrajectory(int n, double x0, double y0,
             }
             ')
 
+# Data frame --------------------------------------------------------------
+
 a=-1.24458046630025
 b=-1.25191834103316
 c=-1.81590817030519
@@ -46,9 +62,7 @@ d=-1.90866735205054
 
 df=createTrajectory(30000, 0, 0, a, b, c, d)
 
-# png("Clifford.png", units="px", width=1600, height=1600, res=300)
-# ggplot(df, aes(x, y)) + geom_point(color="black", shape=46, alpha=.01) + opt
-dev.off()
+# Blanco ------------------------------------------------------------------
 
 p <- ggplot(df, aes(x, y)) + 
   geom_point(color="black", shape=46, alpha=.5) + 
@@ -56,19 +70,34 @@ p <- ggplot(df, aes(x, y)) +
 p
 
 p.sticker <- sticker(
-  p, package=" ", p_size=3, 
-  s_x=1, s_y=1, s_width=1.7, s_height=1.7,
-  h_color = "black", h_fill = "white",
-  filename="img/boxplot-icon-sticker.png"
+  p,
+  package = "Clifford",
+  p_size = 11, p_color = "black", p_family = "mono",
+  p_y = 1.65, 
+  s_x = 1, s_y = 0.85, s_width = 1.4, s_height = 1.4,
+  h_color = "black", h_fill = "white", h_size = 2, 
+  url = "fronkonstin.com", u_size = 2, 
+  filename = "img/clifford-white.png" 
 )
 
+p.sticker
+
+## En negro -------------------------------------
+
+p <- ggplot(df, aes(x, y)) + 
+  geom_point(color="white", shape=46, alpha=.5) + 
+  theme_blicon()
+p
+
 p.sticker <- sticker(
-  p, package="Fronkonstin", 
-  p_size=8, p_color = "black", p_family = "serif", 
-  p_y = 1.6, 
-  s_x=1, s_y=0.85, s_width=1.4, s_height=1.4,
-  h_color = "black", h_fill = "white",
-  filename="img/boxplot-icon-sticker.png"
+  p,
+  package = "Clifford",
+  p_size = 11, p_color = "white", p_family = "mono",
+  p_y = 1.65, 
+  s_x = 1, s_y = 0.85, s_width = 1.4, s_height = 1.4,
+  h_color = "white", h_fill = "black", h_size = 2, white_around_sticker = TRUE, 
+  url = "fronkonstin.com", u_size = 2, 
+  filename = "img/clifford-black.png" 
 )
 
 p.sticker
